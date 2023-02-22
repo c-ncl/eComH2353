a7b4a9d878461c0e0d43207ba343b4bd851af30f
Nicole Bautista <2037770@edu.vaniercollege.qc.ca>
2023-02-22 15:27:50 -0500

    Exported the SQL file.

--- webapplication.sql
+++ webapplication.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2023 at 05:49 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Feb 22, 2023 at 09:27 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,7 +35,7 @@ CREATE TABLE `client` (
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `middle_name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -50,7 +50,7 @@ CREATE TABLE `message` (
   `receiver` int(11) NOT NULL,
   `message` text NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `message`
@@ -63,6 +63,27 @@ INSERT INTO `message` (`message_id`, `sender`, `receiver`, `message`, `timestamp
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `profile_information`
+--
+
+DROP TABLE IF EXISTS `profile_information`;
+CREATE TABLE `profile_information` (
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `profile_information`
+--
+
+INSERT INTO `profile_information` (`user_id`, `first_name`, `middle_name`, `last_name`) VALUES
+(5, 'Jane', 'L.', 'Porter');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -71,7 +92,7 @@ CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password_hash` varchar(72) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
@@ -98,6 +119,12 @@ ALTER TABLE `message`
   ADD PRIMARY KEY (`message_id`),
   ADD KEY `message_to_sender` (`sender`),
   ADD KEY `message_to_receiver` (`receiver`);
+
+--
+-- Indexes for table `profile_information`
+--
+ALTER TABLE `profile_information`
+  ADD UNIQUE KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `user`
@@ -138,6 +165,12 @@ ALTER TABLE `user`
 ALTER TABLE `message`
   ADD CONSTRAINT `message_to_receiver` FOREIGN KEY (`receiver`) REFERENCES `user` (`user_id`),
   ADD CONSTRAINT `message_to_sender` FOREIGN KEY (`sender`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `profile_information`
+--
+ALTER TABLE `profile_information`
+  ADD CONSTRAINT `profile_to_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
