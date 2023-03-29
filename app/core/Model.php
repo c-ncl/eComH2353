59f51387dbd4b3bb9d70d575923be214d85ad8b7
c-ncl <89605841+c-ncl@users.noreply.github.com>
2023-03-29 08:59:34 -0400

    Revert "Self connection, Charset, Composer and a little bit of Codeception."
    This reverts commit eb1f3bffe9eacb1ac08e5d937df45cadddaa587c.

--- app/core/Model.php
+++ app/core/Model.php
@@ -1,27 +1,18 @@
 <?php
 namespace app\core;
-use PDO;
 
 class Model{
-	public static ?PDO $connection  = null;
-
-	public function __construct()
-	{
-		if(self::$connection == null){
-			$host = 'localhost';
-			$dbname = 'webapplication';
-			$user = 'root';
-			$pass = '';
-			$charset = 'utf8mb4';
-
-			try {
-				self::$connection = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $pass);
-				self::$connection->query("SET NAMES $charset");
-			}
-			catch(PDOException $e) {
-				echo $e->getMessage();
-			}
+	public $connection;
+	public function __construct(){
+		$host = 'localhost';
+		$dbname = 'webapplication';
+		$user = 'root';
+		$pass = '';
+		try {
+			$this->connection = new \PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+		}
+		catch(PDOException $e) {
+			echo $e->getMessage();
 		}
 	}
 }
-
--- app/models/User.php
+++ app/models/User.php
@@ -10,7 +10,7 @@ class User extends \app\core\Model
 	public function getByUsername($username)
 	{
 		$SQL = 'SELECT * FROM User WHERE Username = :username';
-		$STH = self::$connection->prepare($SQL);
+		$STH = $this->connection->prepare($SQL);
 		$STH->execute(['username'=>$username]);
 		$STH->setFetchMode(\PDO::FETCH_CLASS, 'app\\models\\User');
 		return $STH->fetch();
@@ -19,10 +19,10 @@ class User extends \app\core\Model
 	public function insert()
 	{
 		$SQL = 'INSERT INTO User(username, password_hash) VALUES (:username, :password_hash)';
-		$STH = self::$connection->prepare($SQL);
+		$STH = $this->connection->prepare($SQL);
 
 		$STH->execute(['username'=>$this->username,
 						'password_hash'=>$this->password_hash]);
-		return self::$connection->lastInsertId(); //return the value of the new PK	
+		return $this->connection->lastInsertId(); //return the value of the new PK	
 	}
 }
